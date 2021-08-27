b52d769cbd0c0dc187cee5c81185526eae6f6690
mathieu abbal <mathieuabbal@hotmail.com>
2021-08-27 15:59:57 +0200

    btn scroll

--- index.js
+++ index.js
@@ -28,5 +28,22 @@ document.getElementById('toggle').addEventListener('click', function() {
 		localStorage.theme = 'dark';
 	}
 });
+//bouton scroll 
+//Récupère le btn
+const back2Top = document.querySelector('#back2Top');
+    back2Top.addEventListener('click', (e) => {
+        e.preventDefault();
+        window.scroll({ top:0, left:0, behavior: 'smooth'});
+    });
+//Lorsque l'utilisateur fait défiler 20px du haut du document vers le bas
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    back2Top.style.display = "block";
+  } else {
+     back2Top.style.display = "none";
+  }
+}
+window.onscroll = function() {scrollFunction()};
+
 
 
